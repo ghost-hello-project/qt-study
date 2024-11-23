c7d4cd4c2d7d53262689554869334cc0567aafc0
khl <xiaodaima2016@163.com>
2024-11-24 03:59:58 +0800

    qt-hello: windows 退出时杀死当前进程

--- qt-hello/src/app/main.cpp
+++ qt-hello/src/app/main.cpp
@@ -2,10 +2,14 @@
 #include <spdlog/spdlog.h>
 
 #include <QApplication>
+#include <QSysInfo>
+#include <cstdlib>
 
 #include "qt-hello/app/mainwindow.h"
 #include "qt-hello/util/app_config.h"
 #include "qt-hello/util/log_util.h"
+
+void exit();
 
 int main(int argc, char *argv[]) {
     janna::LogUtil::init(janna::AppConfig::getInstance()->logLevel(), "../logs/app.log");
@@ -20,5 +24,13 @@ int main(int argc, char *argv[]) {
     SPDLOG_LOGGER_DEBUG(log, fmt::format("janna result code:{}", result));
     log->flush();
     spdlog::shutdown();
+    exit();
     return result;
+}
+
+void exit() {
+#ifdef Q_OS_WIN
+    std::string exit_cmd = "taskkill /t /f /PID " + std::to_string(_getpid());
+    system(exit_cmd.c_str());
+#endif
 }
